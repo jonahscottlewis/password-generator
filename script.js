443a969902a24f9d445080b33fbb287584cacbde
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-01-08 13:59:53 -0800

    added to JS

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function generatePassword(){
+  var userLength = parseInt(prompt("How long?"));
+  var useNumbers = confirm("Would you like to use numbers?");
+  var useLetters = confirm("Would you like to use letters?");
+
+  if (userLength > 8 && userLength < 128) {
+    if (useNumbers || useLetters || lowerLetters || upperLetters)
+  } else {
+    return "Please make length between 8 and 128 characters";
+  }
+
+  return "anything";
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +32,4 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //abcdefghijklmnopqrstuvwxyz0123456789!?
+
