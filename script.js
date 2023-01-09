1a4ef14ef158b66e55656bf3932f20b258c4e2c5
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-01-09 14:53:59 -0800

    added to JavaScript function

--- script.js
+++ script.js
@@ -3,21 +3,51 @@ var generateBtn = document.querySelector("#generate");
 
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"];
+
+var special = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
 
 function generatePassword(){
+  var passwordReturn = [];
   var userLength = parseInt(prompt("How long?"));
-  var useNumbers = confirm("Would you like to use numbers?");
-  var useLetters = confirm("Would you like to use letters?");
+  
 
   if (userLength > 8 && userLength < 128) {
-    if (useNumbers || useLetters || lowerLetters || upperLetters)
-  } else {
-    return "Please make length between 8 and 128 characters";
-  }
+  var useNumbers = confirm("Would you like to use numbers?");
+  var useLetters = confirm("Would you like to use letters?");
+  var useSpecial = confirm("Would you like to use spcial characters?");
+   if (useNumbers || useLetters || useSpecial){
+    var allPossibleCharacters = [];
 
-  return "anything";
-}
+   if (useNumbers){
+    allPossibleCharacters = allPossibleCharacters.concat(numbers);
+   }
+
+   if (useLetters) {
+    allPossibleCharacters = allPossibleCharacters.concat(letters);
+   }
+
+   if (useSpecial) {
+    allPossibleCharacters = allPossibleCharacters.concat(special);
+   }
+
+   for (var i=0; i< userLength; i++){
+    var randomIndex = Math.floor(Math.random() * allPossibleCharacters.length);
+
+    passwordReturn.push(allPossibleCharacters[randomIndex]);
+   }
+
+   return passwordReturn.join("");
+   } else {
+     return "Please select at least one possible set of characters";
+   }
+   } else {
+      return "Please make length between 8 and 128 characters";
+    }
+  };
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -31,5 +61,5 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//abcdefghijklmnopqrstuvwxyz0123456789!?
+
 
